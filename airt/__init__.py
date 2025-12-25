287248f8f35c458b9a76f440acd9ead5eafbfad5
Yugen <omerkorat@gmail.com>
2025-12-25 18:27:52 +0200

    query_llm.py

--- airt/__init__.py
+++ airt/__init__.py
@@ -1,6 +1,6 @@
 from .Agent import Agent
 from .DB import DB
 from .Tool import Tool
-from airt.query_llm import query_llm
+from .query_llm import query_llm
 
 __all__ = ['Agent', 'DB', 'Tool', 'query_llm']
